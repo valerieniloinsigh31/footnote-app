58bf541ea2da4700d74e402bbb5d88cb9408afe7
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-11 23:30:01 +0000

    Add folders and Procfile on top level directory

--- FOOTNOTE_APP/settings.py
+++ FOOTNOTE_APP/settings.py
@@ -21,6 +21,7 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -32,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["FOOTNOTE_APP.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["footnote.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -137,7 +138,6 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
