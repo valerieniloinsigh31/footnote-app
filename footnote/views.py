641b18f4322c0425c90e26ea2ba2d0e88798f980
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-12 14:58:29 +0000

    Create view for IdeaList-specifying one idea per page

--- footnote/views.py
+++ footnote/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Idea 
 
-# Create your views here.
+
+class IdeaList(generic.ListView):
+    model = Idea
+    queryset = Idea.objects.filter(status=1).order_by(created_on)
+    template_name  = 'index.html'
+    paginate_by = 1
