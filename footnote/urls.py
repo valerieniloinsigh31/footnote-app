caab9757e0071899f8e1dcef3fc45c02838f50e1
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-12 20:11:49 +0000

    Connect up urls in the urls.py file

--- footnote/urls.py
+++ footnote/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path 
 
 urlpatterns = [
-    path('', views.IdeaList.as_view(), name='home')
+    path('', views.IdeaList.as_view(), name='home'),
+    path('<slug:slug/>', views.IdeaDetail.as_view(), name='idea_detail')
 ]
