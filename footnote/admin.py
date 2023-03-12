37b1cd67f0396a514907a23d87ab232673646daf
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-12 14:17:57 +0000

    Add admin registration for FootNotes class

--- footnote/admin.py
+++ footnote/admin.py
@@ -5,5 +5,15 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Idea)
 class IdeaAdmin(SummernoteModelAdmin):
 
+    list_display = {'title','slug','status','created_on'}
+    search_fields = ['title','content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
 
+@admin.register(FootNote)
+class FootNoteAdmin(admin.ModelAdmin):
+
+    list_display = ('name','body','post','created_on','approved')
+    list_filter = ('approved','created_on')
+    search_fields =('name','email','body')
