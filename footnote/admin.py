7d1046e83098f1cedd933ed720b6f56c9b6bcae0
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-12 02:54:23 +0000

    Add admin function for defined model classes Idea and FootNote

--- footnote/admin.py
+++ footnote/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Idea, FootNote
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Idea)
+@admin.register(Idea)
+class IdeaAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+
