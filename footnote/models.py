b0eb10d0143cc0d9bae9890ddd12765e70d4e665
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-12 01:12:57 +0000

    Add comments model

--- footnote/models.py
+++ footnote/models.py
@@ -16,3 +16,14 @@ class Post(models.Model):
     deleted_on = models.DateTimeField(auto_now_deleted=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='footnote_likes', blank=True)
+
+    class Meta():
+        ordering = ['-created_on']
+
+    def __str__ (self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Comment(models.Model):
