2e9a116c71eec32a5d388b8859303c62d71cce9a
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-13 01:12:34 +0000

    Update url path for idea_like form

--- footnote/urls.py
+++ footnote/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.IdeaList.as_view(), name='home'),
     path('<slug:slug>/', views.IdeaDetail.as_view(), name='idea_detail'),
+    path('like/<slug:slug>', views.IdeaLike.as_view(), name='idea_like'),
 ]
