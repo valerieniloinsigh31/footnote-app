dbac0365fc4200b7eeae40147d9cf5c80f4b8676
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-14 19:21:41 +0000

    Add likes to both ideas and footnotes

--- footnote/models.py
+++ footnote/models.py
@@ -14,7 +14,7 @@ class Idea(models.Model):
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='footnote_likes', blank=True)
+    likes = models.ManyToManyField(User, related_name='idea_likes', blank=True)
 
     class Meta():
         ordering = ['-created_on']
@@ -40,6 +40,7 @@ class FootNote(models.Model):
         return f"FootNote {self.body} given by {self.name}"
 
 
-class 
+class Store(model.Models):
+
 
    
