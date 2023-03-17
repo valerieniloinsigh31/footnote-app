85f0f35f50bdfab4223f07802f29f08b87c83496
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-17 22:03:14 +0000

    Create media folder

--- FOOTNOTE_APP/settings.py
+++ FOOTNOTE_APP/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["footnote.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ['footnote.herokuapp.com', 'localhost']
 
 
 # Application definition
