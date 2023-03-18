f92ddb72448aab16c4c7749caa4422e4da35f994
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-18 12:26:45 +0000

    Amended tiktok icon

--- footnote/urls.py
+++ footnote/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.IdeaList.as_view(), name='home'),
     path('<slug:slug>/', views.IdeaDetail.as_view(), name='idea_detail'),
     path('', views.MedleyDetail.as_view(), name='medley'),
-    path('', views.MedleyDetail.as_view(), name='medley_detail'),
     path('like/<slug:slug>', views.IdeaLike.as_view(), name='idea_like'),
     path('like/<slug:slug>', views.FootnoteLike.as_view(), name='footnote_like'),
 ]
