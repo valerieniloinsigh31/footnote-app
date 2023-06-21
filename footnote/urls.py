d4bfd46c5a342162b3b60ec3cc721e84ff9b1499
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-06-21 11:05:22 +0000

    Add writerprofile link to navbar

--- footnote/urls.py
+++ footnote/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path 
 
 urlpatterns = [
-    path('', views.IdeaList.as_view(), name='home'),  # UPDATE home to footnote as home app is called footnote
+    path('', views.IdeaList.as_view(), name='home'),  # Consider updating home
     path('<slug:slug>/', views.IdeaDetail.as_view(), name='idea_detail'),
     path('like/<slug:slug>', views.IdeaLike.as_view(), name='idea_like'),
 ]
