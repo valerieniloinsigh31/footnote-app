971ddeb6972ee26986882f6869a563d18f0855bb
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-06-24 18:24:41 +0000

    Move idea_detail html file into footnote app templates subfolder

--- FOOTNOTE_APP/settings.py
+++ FOOTNOTE_APP/settings.py
@@ -25,7 +25,7 @@ DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['footnote.herokuapp.com', '8000-valerienilo-footnoteapp-7lio4udpzq9.ws-eu100.gitpod.io']
+ALLOWED_HOSTS = ['footnote.herokuapp.com', '8000-valerienilo-footnoteapp-rudralocqle.ws-eu100.gitpod.io']
 
 
 # Application definition
