3c0ffaa35f18730a74cd4ea2e1f29d9f761b1c40
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-07-01 09:07:39 +0000

    Amend carousel on medley page by replacing bootrap four code with bootstrap five-version of bootstrap used confirmed by checking base.html

--- writer_profile/static/writer_profile/js/writerprofile.js
+++ writer_profile/static/writer_profile/js/writerprofile.js
@@ -1,6 +1,6 @@
 const starWrapper = document.querySelector('.stars');
 const stars = document.querySelectorAll('.stars a');
-
+//console.log("connected");
 stars.forEach((star, clickedIdx) => {
 star.addEventListener('click', () => {
     starWrapper.classList.add('disabled');
