8bf777c14d7e9485242e1a51281adf44bbcfaf0d
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-07-01 22:16:38 +0000

    Set debug to false and DISABLE_COLLECTSTATIC removal from heorku-check deployment

--- FOOTNOTE_APP/settings.py
+++ FOOTNOTE_APP/settings.py
@@ -21,7 +21,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
@@ -155,7 +155,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/' # Name of images folder in static
+MEDIA_URL = '/media/' 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
